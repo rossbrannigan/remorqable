e6b50f0b38279c394d7e03e43fbbc952ec2d3a0c
Ross Brannigan 2018 <rossbrannigan2018@Mac.lan>
2026-02-22 18:19:33 +0300

    Ignore Next.js build output and macOS files

--- .next/dev/server/chunks/_next-internal_server_app_api_videos_route_actions_d409b075.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/videos/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_videos_route_actions_d409b075.js.map
--- .next/dev/static/chunks/app_page_tsx_893e5ca7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_page_tsx_dda9881a._.js",
-  "static/chunks/node_modules_241e5298._.js"
-],
-    source: "dynamic"
-});
